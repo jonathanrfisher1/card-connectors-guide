0f93d5f91426bba2002c0cb350e2376017e77a8e
Fisher (Jonathan) <fisherj@vmware.com>
2018-08-15 19:29:30 -0400

    Weather connector will now request and log all environment variables.

--- samples/node/routes/weather.js
+++ samples/node/routes/weather.js
@@ -5,6 +5,8 @@
 
 "use strict";
 
+
+
 const uuidV4 = require('uuid/v4');
 
 exports.requestCards = function(req, res) {
@@ -12,6 +14,18 @@ exports.requestCards = function(req, res) {
         res.status(400).send("Missing tokens field");
         return;
     }
+
+    console.log("user_email", req.body.tokens.user_email);
+    console.log("enrollment_identifier", req.body.tokens.enrollment_identifier);
+    console.log("enrollment_username", req.body.tokens.enrollment_username);
+    console.log("enrollment_group_identifier", req.body.tokens.enrollment_group_identifier);
+    console.log("enrollment_device_udid", req.body.tokens.enrollment_device_udid);
+    console.log("device_identifier", req.body.tokens.device_identifier);
+    console.log("device_model", req.body.tokens.device_model);
+    console.log("device_name", req.body.tokens.device_name);
+    console.log("device_system_name", req.body.tokens.device_system_name);
+    console.log("device_system_version", req.body.tokens.device_system_version);
+    console.log("device_ui_idiom", req.body.tokens.device_ui_idiom);
 
     // Treat missing zips and empty zips the same way
     const zips = req.body.tokens.zip || [];
@@ -21,7 +35,7 @@ exports.requestCards = function(req, res) {
     const routingPrefix = req.headers['x-routing-prefix'] || '/';
 
     res.json({cards: zips.map(function(zip){
-      return toCard(zip, routingPrefix);
+      return toCard(zip, routingPrefix, req.body.tokens);
     })});
 };
 
@@ -34,7 +48,7 @@ exports.reportWeather = function (req, res) {
     res.status(200).end();
 };
 
-function toCard(zip, routingPrefix) {
+function toCard(zip, routingPrefix, tokens) {
     return {
         id: uuidV4(),
         template: {
@@ -49,7 +63,7 @@ function toCard(zip, routingPrefix) {
                 {
                     type: "GENERAL",
                     title: "Temperature",
-                    description: "75"
+                    description: tokens.user_email[0]
                 }, {
                     type: "GENERAL",
                     title: "Conditions",
